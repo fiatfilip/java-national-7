1b6c6eaaa2e4aad4a6a5915467070faf2ba1b425
StefanHorodnic <stefanhorodnic@yahoo.com>
2021-10-07 11:43:07 +0300

    Coding Clash Solution 2

---
+++ src/main/java/ro/siit/session30/Solution2.java
@@ -0,0 +1,39 @@
+package ro.siit.session30;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Solution2 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int k = in.nextInt();
+
+        Integer[] bisquitsTypesCont = new Integer[k];
+
+        for (int i = 0; i < k; i++) {
+            int q = in.nextInt();
+            bisquitsTypesCont[i] = q;
+        }
+
+        Arrays.sort(bisquitsTypesCont, Collections.reverseOrder());
+
+        int maxDays = 0;
+
+        for(int i = 1; i <= k; i++){
+            for(int j = 0; j < i; j++) {
+                bisquitsTypesCont[j]--;
+            }
+            maxDays++;
+            Arrays.sort(bisquitsTypesCont, Collections.reverseOrder());
+            if(bisquitsTypesCont[k-1].equals(0)){
+                break;
+            }
+        }
+
+        System.out.println(maxDays);
+    }
+
+}
